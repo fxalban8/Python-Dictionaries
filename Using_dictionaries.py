f77469c735076d8097cc51956e38a7eb05e6ece4
fxalban8 <francisco.alban@epn.edu.ec>
2020-05-25 20:54:22 -0500

    The dictionary has been printed

--- Using_dictionaries.py
+++ Using_dictionaries.py
@@ -1,3 +1,5 @@
 #Dictionaries are unordered list of objects
 #Let's create a dictionary
 LATAM_presidents={"Brasil":"Bolsonaro", "Colombia": "Duque", "Ecuador": "Moreno", "Peru": "Vizcarra"}
+#we can print the dictionary by using the print command
+print(LATAM_presidents)
